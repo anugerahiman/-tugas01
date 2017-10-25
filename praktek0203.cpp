9d3e32d7b5452faea3176f9942fc81e887ed5ac0
krisdianagus <32463039+krisdianagus@users.noreply.github.com>
2017-10-25 20:16:47 +0800

    menghitung dan menampilkan jumlah kontak dalam1 ton

---
+++ praktek0203.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){	
+	double ton=35273.92;
+	
+	double berat;
+	cin >> berat;
+	
+	double ounce = 1/ton;
+	double beratTon = berat*ounce;
+	
+	cout << beratTon<<endl;
+	
+	double jumlah=1/beratTon;
+	cout << jumlah;
+}
